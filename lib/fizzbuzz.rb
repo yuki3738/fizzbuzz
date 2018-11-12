b15a99783c946225248aa1549db28c4a87f4444e
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-12 22:55:41 +0900

    Fizzbuzz.fizzbuzzメソッドを実装

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,17 @@
 require "fizzbuzz/version"
 
 module Fizzbuzz
-  # Your code goes here...
+  def self.fizzbuzz(n)
+    1.upto(n).map do |i|
+      if i % 15 == 0
+        'fizzbuzz'
+      elsif i % 3 == 0
+        'fizz'
+      elsif i % 5 == 0
+        'buzz'
+      else
+        i
+      end
+    end
+  end
 end
