7a87de3c04a2bf0313eb130358a1dec88107ad49
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-15 21:28:24 +0900

    Fizzbuzz.fizzbuzzのtestを追加

--- test/fizzbuzz_test.rb
+++ test/fizzbuzz_test.rb
@@ -1,11 +1,12 @@
 require "test_helper"
 
 class FizzbuzzTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Fizzbuzz::VERSION
+  def test_fizzbuzz_1
+    assert_equal [1], Fizzbuzz.fizzbuzz(1)
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_fizzbuzz_15
+    assert_equal [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"],
+      Fizzbuzz.fizzbuzz(15)
   end
 end
